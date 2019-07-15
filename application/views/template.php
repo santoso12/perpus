19108ffb3ebeebc49ea3521a30f22fcba9bf5ad9
juprisantoso <juprisantoso@student.uns.ac.id>
2019-07-15 13:44:31 +0700

    small update template

--- application/views/template.php
+++ application/views/template.php
@@ -134,9 +134,7 @@
             </ul>
           </li>
           <!-- Control Sidebar Toggle Button -->
-          <li>
-            <a href="#" data-toggle="control-sidebar"><i class="fa fa-gears"></i></a>
-          </li>
+          
         </ul>
       </div>
     </nav>
@@ -170,17 +168,9 @@
       <ul class="sidebar-menu">
         <li class="header">MAIN NAVIGATION</li>
         <li class="treeview">
-          <a href="#">
+          <a href="<?php echo site_url('dashboard'); ?>">
             <i class="fa fa-dashboard"></i> <span>Dashboard</span>
-            <span class="pull-right-container">
-              <i class="fa fa-angle-left pull-right"></i>
-            </span>
           </a>
-          <ul class="treeview-menu active">
-            <li><a href="../index.html"><i class="fa fa-circle-o"></i> Dashboard v1</a></li>
-            <li><a href="../index2.html"><i class="fa fa-circle-o"></i> Dashboard v2</a></li>
-          </ul>
-        </li>
         <li class="treeview">
           <a href="#">
             <i class="fa fa-files-o"></i>
