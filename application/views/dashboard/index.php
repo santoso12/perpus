d5f568645a3539f12684f3ccc01df615ded2482a
juprisantoso <juprisantoso@student.uns.ac.id>
2019-07-26 14:52:28 +0700

    update

--- application/views/dashboard/index.php
+++ application/views/dashboard/index.php
@@ -10,14 +10,15 @@
 </section>
 <!-- Main content -->
 <section class="content">
-
+<div class="row">
+<div class="col-md-8">
 <div class="box box-primary box-solid">
 	<div class="box-header with-border">
 		<h3 class="box-title"><i class="fa fa-bookmark">&nbsp;</i> Publikasi</h3>
 	  <!-- /.box-tools -->
 	</div>
 	<!-- /.box-header -->
-	<div class="box-body">
+	<div class="box-body text-center">
 		<p style="font-size:18px;">Apakah anda sudah melakukan publikasi karya ilmiah dalam bentuk jurnal atau prosiding?</p><br>
 		
 		<a href="<?php echo site_url('publikasi/sudah'); ?>" class="btn btn-primary">
@@ -27,6 +28,8 @@
 		<a href="<?php echo site_url('publikasi/belum'); ?>" class="btn btn-warning">Belum</a>
 	</div>
 <!-- /.box-body -->
+</div>
+</div>
 </div>
 
 </section>
--- application/views/template.php
+++ application/views/template.php
@@ -30,7 +30,7 @@
 
   <header class="main-header">
     <!-- Logo -->
-    <a href="../index2.html" class="logo">
+    <a href="<?php echo base_url() ?>" class="logo">
       <!-- mini logo for sidebar mini 50x50 pixels -->
       <span class="logo-mini"><b>BP</b></span>
       <!-- logo for regular state and mobile devices -->
@@ -49,84 +49,29 @@
       <div class="navbar-custom-menu">
         <ul class="nav navbar-nav">
           <!-- Notifications: style can be found in dropdown.less -->
-          <li class="dropdown notifications-menu">
-            <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-              <i class="fa fa-bell-o"></i>
-              <span class="label label-warning">10</span>
-            </a>
-            <ul class="dropdown-menu">
-              <li class="header">You have 10 notifications</li>
-              <li>
-                <!-- inner menu: contains the actual data -->
-                <ul class="menu">
-                  <li>
-                    <a href="#">
-                      <i class="fa fa-users text-aqua"></i> 5 new members joined today
-                    </a>
-                  </li>
-                  <li>
-                    <a href="#">
-                      <i class="fa fa-warning text-yellow"></i> Very long description here that may not fit into the
-                      page and may cause design problems
-                    </a>
-                  </li>
-                  <li>
-                    <a href="#">
-                      <i class="fa fa-users text-red"></i> 5 new members joined
-                    </a>
-                  </li>
-                  <li>
-                    <a href="#">
-                      <i class="fa fa-shopping-cart text-green"></i> 25 sales made
-                    </a>
-                  </li>
-                  <li>
-                    <a href="#">
-                      <i class="fa fa-user text-light-blue"></i> You changed your username
-                    </a>
-                  </li>
-                </ul>
-              </li>
-              <li class="footer"><a href="#">View all</a></li>
-            </ul>
-          </li>
+          
 
           <!-- User Account: style can be found in dropdown.less -->
           <li class="dropdown user user-menu">
             <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-              <img src="<?php echo base_url() ?>assets/img/user2-160x160.jpg" class="user-image" alt="User Image">
+              <img src="<?php echo base_url() ?>assets/img/user.png" class="user-image" alt="User Image">
               <span class="hidden-xs"><?php echo $this->session->userdata('nama'); ?></span>
             </a>
             <ul class="dropdown-menu">
               <!-- User image -->
               <li class="user-header">
-                <img src="<?php echo base_url() ?>assets/img/user2-160x160.jpg" class="img-circle" alt="User Image">
+                <img src="<?php echo base_url() ?>assets/img/user.png" class="img-circle" alt="User Image">
 
                 <p>
                   <?php echo $this->session->userdata('nama'); ?>
-                  <small>Member since Nov. 2012</small>
+				  <small> <?php echo $this->session->userdata('email'); ?></small>
                 </p>
               </li>
               <!-- Menu Body -->
-              <li class="user-body">
-                <div class="row">
-                  <div class="col-xs-4 text-center">
-                    <a href="#">Followers</a>
-                  </div>
-                  <div class="col-xs-4 text-center">
-                    <a href="#">Sales</a>
-                  </div>
-                  <div class="col-xs-4 text-center">
-                    <a href="#">Friends</a>
-                  </div>
-                </div>
-                <!-- /.row -->
-              </li>
+              
               <!-- Menu Footer-->
               <li class="user-footer">
-                <div class="pull-left">
-                  <a href="#" class="btn btn-default btn-flat">Profile</a>
-                </div>
+                
                 <div class="pull-right">
                   <a href="#" class="btn btn-default btn-flat">Sign out</a>
                 </div>
@@ -146,30 +91,20 @@
       <!-- Sidebar user panel -->
       <div class="user-panel">
         <div class="pull-left image">
-          <img src="<?php echo base_url() ?>assets/img/user2-160x160.jpg" class="img-circle" alt="User Image">
+          <img src="<?php echo base_url() ?>assets/img/user.png" class="img-circle" alt="User Image">
         </div>
         <div class="pull-left info">
           <p><?php echo $this->session->userdata('nama'); ?></p>
           <a href="#"><i class="fa fa-circle text-success"></i> Online</a>
         </div>
       </div>
-      <!-- search form -->
-      <form action="#" method="get" class="sidebar-form">
-        <div class="input-group">
-          <input type="text" name="q" class="form-control" placeholder="Search...">
-              <span class="input-group-btn">
-                <button type="submit" name="search" id="search-btn" class="btn btn-flat"><i class="fa fa-search"></i>
-                </button>
-              </span>
-        </div>
-      </form>
-      <!-- /.search form -->
+      
       <!-- sidebar menu: : style can be found in sidebar.less -->
       <ul class="sidebar-menu">
-        <li class="header">MAIN NAVIGATION</li>
+        <li class="header">MENU UTAMA</li>
         <li class="treeview">
           <a href="<?php echo site_url('dashboard'); ?>">
-            <i class="fa fa-dashboard"></i> <span>Dashboard</span>
+            <i class="fa fa-dashboard"></i> <span>Home</span>
           </a>
         <li class="treeview">
           <a href="#">
@@ -180,7 +115,7 @@
             </span>
           </a>
           <ul class="treeview-menu">
-            <li><a href="<?php echo site_url('eprints') ?>"><i class="fa fa-circle-o"></i> Account</a></li>
+            <?php /* <li><a href="<?php echo site_url('eprints') ?>"><i class="fa fa-circle-o"></i> Account</a></li> */ ?>
             <li><a href="<?php echo site_url('eprints/panduan') ?>"><i class="fa fa-circle-o"></i> Panduan Upload Mandiri</a></li>
 			 <li><a href="#"><i class="fa fa-circle-o"></i> Status</a></li>
           </ul>
